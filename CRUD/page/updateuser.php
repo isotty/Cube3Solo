fa224883880d117c4c8b9af68b7a55d361a73602
isotty <valentin.mahieux@viacesi.fr>
2022-06-01 17:52:49 +0200

    update

--- CRUD/page/updateuser.php
+++ CRUD/page/updateuser.php
@@ -13,19 +13,19 @@
 
 <form class="formulaire" action="../controller/updateuser.php" method="post">
   <div>
-    <span for="">Nom : </span>
+    <span>Nom : </span>
     <input type="text" name="lastname" value="<?php echo $user["lastname"] ?>" class="" id="lastname" placeholder="Entrez le nom de la personne">
   </div>
   <div>
-    <span for="exampleFormControlInput2">Prénom : </span>
+    <span>Prénom : </span>
     <input type="text" name="firstname" value="<?php echo $user["firstname"] ?>" class="" id="firstname" placeholder="Entrez le prénom de la personne">
   </div>
   <div>
-    <span for="">Email : </span>
+    <span>Email : </span>
     <input type="email" name="email" value="<?php echo $user["email"] ?>" class="" id="email" placeholder="Entrez l'email de la personne">
   </div>
   <div>
-    <span for="">Mot de passe: </span>
+    <span>Mot de passe: </span>
     <input type="password" name="password" value="<?php echo $user["pdw"] ?>" class="" id="pwd" placeholder="Entrez un mot de passe pour la personne">
   </div>
   <input type="hidden" name="id" value="<?php echo $_POST['id'] ?>">
