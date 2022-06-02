9f3368ac87073d2049f2e92a4f021923e976ee6b
isotty <valentin.mahieux@viacesi.fr>
2022-06-02 09:27:42 +0200

    Update updateuser.php

--- CRUD/page/updateuser.php
+++ CRUD/page/updateuser.php
@@ -6,6 +6,15 @@
     $data = $check->fetch();
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style/index.css">
+    <title>Connexion</title>
+</head>
+
 <form class="formulaire" action="../controller/updateuser.php" method="post">
   <div>
     <span>Nom : </span>
@@ -29,3 +38,4 @@
       <button type="submit"  class="">Envoyer</button>
   </div>
 </form>
+</html>
