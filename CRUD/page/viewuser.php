454fce3284365082dda2fe821cb12031f7b77771
isotty <valentin.mahieux@viacesi.fr>
2022-06-02 08:49:03 +0200

    Update viewuser.php

--- CRUD/page/viewuser.php
+++ CRUD/page/viewuser.php
@@ -25,24 +25,24 @@ function getUsers(){
     <nav>
 
     </nav>
-    <form>
+    <form class="container">
         <a href='.\createuser.php' class="" >Ajouter nouvelle utilisateur</a>
         <?php 
             foreach($users as $user):
             extract($user);
         ?>
-        <li id="<?php echo $id ?>">
-            <span><?php echo $firstname ?></span>
-            <span><?php echo $lastname ?></span>
-            <span><?php echo $email ?></span>
+        <div class="row" id="<?php echo $id ?>">
+            <div class="col"><?php echo $firstname ?></div>
+            <div class="col"><?php echo $lastname ?></div>
+            <div class="col"><?php echo $email ?></div>
             <form method="POST" action="./updateuser.php">
-                <button class="btn" type="submit" name="id" value="<?php echo $id ?>">Modifier</button>
+                <button class="btn btn-primary" type="submit" name="id" value="<?php echo $id ?>">Modifier</button>
             </form>
             <form method="POST" action="../controller/deleteuser.php">
-                <button class="btn" type="submit" name="id" value="<?php echo $id ?>">Supprimer</button>
+                <button class="btn btn-primary" type="submit" name="id" value="<?php echo $id ?>">Supprimer</button>
             </form>
-        </li>
+        </div>
         <?php endforeach; ?>
-    </form>
+    </form class="container">
 </body>
 </html>
